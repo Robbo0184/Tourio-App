4d4b71857ac7a49c811156e777578e00e207b1a9
Royce Brouër <royce@systemli.org>
2023-10-10 15:39:43 +0200

    implement place model

---
+++ db/models/Place.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const placeSchema = new Schema({
+  name: String,
+  location: String,
+  image: String,
+  mapURL: String,
+  description: String,
+  //comments: {type: [Schema.Types.ObjectId], ref: "Comment" }
+});
+
+const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
+
+export default Place;
